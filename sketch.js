a6015192b49b1f2b629288dc09b5dd943cf56e80
René Martínez <hola@r3n0.com>
2023-02-04 08:05:22 -0500

    éxito visualizando

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let data;
 let columns;
 
 function preload() {
-	data = loadTable('./src/30canciones.csv', 'csv', 'header');
+	data = loadTable('./src/hts.csv', 'csv', 'header');
 }
 function setup() {
 	angleMode(DEGREES);
@@ -18,7 +18,7 @@ function draw() {
 	let celWidth = (width - margin) / data.columns.length;
 	let celHeight = (height - margin) / data.rows.length;
 
-	for (let j = 0; j < data.columns.length; j++) {
+	for (let j = 1; j < data.columns.length; j++) {
 		const x = j * celWidth + margin / 2;
 		push();
 		translate(x, height - 20);
@@ -28,21 +28,31 @@ function draw() {
 		text(data.columns[j].slice(0, 30), 0, 0);
 		pop();
 
-		const c = color(random(0, 255), random(0, 255), 0, 50);
+		const c = color(random(0, 255), random(0, 255), 0);
 		stroke(c);
 		for (let i = 0; i < data.rows.length; i++) {
 			const count = data.rows[i].obj[data.columns[j]];
-			const value = count ? int(count) / 40000 : 0;
+			let value = count ? int(count) : 0;
 
 			const y = i * celHeight + margin / 2;
+
+			const maxLines = 30;
+
+			if (j == 1) {
+				value = map(value, 45, 58, 1, maxLines);
+			} else if (j == 2) {
+				value = map(value, 25, 31, 1, maxLines);
+			} else if (j == 3) {
+				value = map(value, 36, 66, 1, maxLines);
+			}
 
 			// text(int(value), x, y);
 			for (let k = 0; k < value; k++) {
 				const v = p5.Vector.random2D();
-				v.setMag(8 + (value / 2) * random(0.4, 1));
+				v.setMag(value * random(0.4, 1));
 				push();
 				translate(x - v.x / 2, y - v.y / 2);
-				line(0, 0, v.x, v.y);
+				line(0, 0, v.x * 3, v.y);
 				pop();
 			}
 			// save(`mySVG${i}.svg`);
